5ebc55f1a4ca48d9c9e009bb5ce2238b85bbcadf
Mike Dillon <mike@appropriate.io>
2015-09-05 21:44:54 -0700

    Allow OWNCLOUD_TLS_PORT to be specified
    This is needed in the case that container port 443 is published as a different
    host port

--- 8.1/docker-entrypoint.sh
+++ 8.1/docker-entrypoint.sh
@@ -9,6 +9,7 @@ fi
 if [[ "$1" = apache2* ]]; then
 	: ${OWNCLOUD_TLS_CERT:=$OWNCLOUD_SSL_CERT}
 	: ${OWNCLOUD_TLS_KEY:=$OWNCLOUD_SSL_KEY}
+	: ${OWNCLOUD_TLS_PORT:=443}
 
 	if [ "$OWNCLOUD_TLS_CERT" -o "$OWNCLOUD_TLS_KEY" ]; then
 		if [ -z "$OWNCLOUD_TLS_CERT" -o -z "$OWNCLOUD_TLS_KEY" ]; then
@@ -20,25 +21,26 @@ if [[ "$1" = apache2* ]]; then
 			echo >&2 "ERROR: TLS certificate '$OWNCLOUD_TLS_CERT' not found"
 			exit 1
 		elif ! [ -f "$OWNCLOUD_TLS_CERT" ]; then
-			export OWNCLOUD_TLS_CERT="/etc/ssl/certs/$OWNCLOUD_TLS_CERT"
+			OWNCLOUD_TLS_CERT="/etc/ssl/certs/$OWNCLOUD_TLS_CERT"
 		fi
 
 		if ! [ -f "$OWNCLOUD_TLS_KEY" -o -f "/etc/ssl/private/$OWNCLOUD_TLS_KEY" ]; then
 			echo >&2 "ERROR: TLS key '$OWNCLOUD_TLS_KEY' not found"
 			exit 1
 		elif ! [ -f "$OWNCLOUD_TLS_KEY" ]; then
-			export OWNCLOUD_TLS_KEY="/etc/ssl/private/$OWNCLOUD_TLS_KEY"
+			OWNCLOUD_TLS_KEY="/etc/ssl/private/$OWNCLOUD_TLS_KEY"
 		fi
 
 		a2enmod ssl
 		a2ensite owncloud-ssl
-	else
-		cat >&2 <<-'EOWARN'
-		WARNING: Running ownCloud without HTTPS is not recommended.
-		         See the documentation for information on enabling HTTPS:
 
-			 https://github.com/docker-library/docs/tree/master/owncloud
-		EOWARN
+		export OWNCLOUD_TLS_CERT OWNCLOUD_TLS_KEY OWNCLOUD_TLS_PORT
+	else
+		echo >&2 'WARNING: Running ownCloud without HTTPS is not recommended.'
+		echo >&2 '         See the documentation for information on enabling HTTPS:'
+		echo >&2
+		echo >&2 '         https://github.com/docker-library/docs/tree/master/owncloud'
+		echo >&2
 	fi
 fi
 
